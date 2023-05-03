565986dcbcbab4c2a5c59ab817a69d5b6ad29c62
Rhythima Pandey <92260206+rhythima@users.noreply.github.com>
2023-05-03 20:09:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+/* eslint-disable react-native/no-inline-styles */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {View} from 'react-native';
+import { AudioPlayer } from './src/AudioPlayer';
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#ACB1D6',
+        justifyContent: 'center',
+      }}>
+      <AudioPlayer
+        url={'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3'}
+      />
+    </View>
+  );
+};
+
+export default App;
